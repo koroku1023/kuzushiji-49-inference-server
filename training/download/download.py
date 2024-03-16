b8fd2a6bbee5022261fd25a37439b9a934672491
koroku1023 <h1023skoroku@gmail.com>
2024-03-17 02:13:14 +0900

    feat: add script for downloading classmap in download.py resf: 02_data_analysis

--- training/download/download.py
+++ training/download/download.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import pandas as pd
 
 try:
     from tqdm import tqdm
@@ -18,6 +19,7 @@ url_list = [
     "http://codh.rois.ac.jp/kmnist/dataset/k49/k49-train-labels.npz",
     "http://codh.rois.ac.jp/kmnist/dataset/k49/k49-test-imgs.npz",
     "http://codh.rois.ac.jp/kmnist/dataset/k49/k49-test-labels.npz",
+    "http://codh.rois.ac.jp/kmnist/dataset/k49/k49_classmap.csv",
 ]
 
 
@@ -26,22 +28,26 @@ def download_list(url_list):
     for url in url_list:
         file_name = url.split("/")[-1]
         path = os.path.join("data/raw/", file_name)
-        r = requests.get(url, stream=True)
-        with open(path, "wb") as f:
-            total_length = int(r.headers.get("content-length"))
-            print(
-                "Downloading {} - {:.1f} MB".format(
-                    path, (total_length / 1024000)
+        if not file_name.endswith(".csv"):
+            r = requests.get(url, stream=True)
+            with open(path, "wb") as f:
+                total_length = int(r.headers.get("content-length"))
+                print(
+                    "Downloading {} - {:.1f} MB".format(
+                        path, (total_length / 1024000)
+                    )
                 )
-            )
 
-            for chunk in tqdm(
-                r.iter_content(chunk_size=1024),
-                total=int(total_length / 1024) + 1,
-                unit="KB",
-            ):
-                if chunk:
-                    f.write(chunk)
+                for chunk in tqdm(
+                    r.iter_content(chunk_size=1024),
+                    total=int(total_length / 1024) + 1,
+                    unit="KB",
+                ):
+                    if chunk:
+                        f.write(chunk)
+        else:
+            df = pd.read_csv(url)
+            df.to_csv(path, index=False)
     print("All dataset files downloaded!")
 
 
